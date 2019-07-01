3a036ad5755ea223d9fa3ea0fbf6210c61360add
Frode <frode@fintlabs.no>
2019-07-01 23:46:04 +0200

    added controller specs

--- src/test/groovy/no/fint/kulturtanken/ControllerSpec.groovy
+++ src/test/groovy/no/fint/kulturtanken/ControllerSpec.groovy
@@ -2,6 +2,10 @@ package no.fint.kulturtanken
 
 
 import no.fint.test.utils.MockMvcSpecification
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.web.reactive.function.client.WebClient
 
@@ -10,21 +14,35 @@ class ControllerSpec extends MockMvcSpecification {
     private MockMvc mockMvc
     private FintService fintService
     private Controller controller
-    private WebClient webClient = Mock(WebClient)
+    private def server = new MockWebServer()
 
     void setup() {
-        fintService = new FintService(webClient: webClient)
+        fintService = new FintService(webClient: WebClient.create(server.url('/').toString()))
         controller = new Controller(fintService: fintService)
         mockMvc = standaloneSetup(controller)
     }
 
     def "When some of the backend data returns 404 the controller should return 404 "() {
+        given:
+        server.enqueue(new MockResponse().setResponseCode(HttpStatus.NOT_FOUND.value()))
 
         when:
-        def response = mockMvc.perform(get('api/skoleorganisasjon'))
+        def response = mockMvc.perform(get('/api/skoleorganisasjon').header(HttpHeaders.AUTHORIZATION, "bearer"))
 
         then:
-        0 * webClient.get() >> new URINotFoundException("test")
+
         response.andExpect(status().isNotFound())
+    }
+
+    def "When some of the backend services times out return 408"() {
+        given:
+        server.enqueue(new MockResponse().setResponseCode(HttpStatus.REQUEST_TIMEOUT.value()))
+
+        when:
+        def response = mockMvc.perform(get('/api/skoleorganisasjon').header(HttpHeaders.AUTHORIZATION, "bearer"))
+
+        then:
+
+        response.andExpect(status().isRequestTimeout())
     }
 }
