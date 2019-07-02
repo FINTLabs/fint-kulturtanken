1febff6a5ea1bbfa22b1f42165c6388546e56d71
Ola <olastaalberg@gmail.com>
2019-07-02 09:21:54 +0200

    changed string in tests

--- src/test/groovy/no/fint/kulturtanken/ControllerSpec.groovy
+++ src/test/groovy/no/fint/kulturtanken/ControllerSpec.groovy
@@ -9,20 +9,22 @@ class ControllerSpec extends MockMvcSpecification {
     private MockMvc mockMvc
     private FintService mockService
     private Controller controller
+    private String bearer
 
     void setup() {
         mockService = Mock(FintService)
         controller = new Controller(fintService: mockService)
         mockMvc = standaloneSetup(controller)
+        bearer = "test"
     }
 
     def "When some of the backend data returns 404 the controller should return 404 "() {
         given:
-        1 * mockService.getSkoleOrganisasjon("test") >> { throw new URINotFoundException("test") }
+        1 * mockService.getSkoleOrganisasjon(bearer) >> { throw new URINotFoundException("test") }
 
         when:
         def response = mockMvc.perform(get('/api/skoleorganisasjon/')
-                .header(HttpHeaders.AUTHORIZATION, "test"))
+                .header(HttpHeaders.AUTHORIZATION, bearer))
 
         then:
         response.andExpect(status().isNotFound())
@@ -30,11 +32,11 @@ class ControllerSpec extends MockMvcSpecification {
 
     def "When some backend returns 400 controller should return 400 Bad Request"() {
         given:
-        1 * mockService.getSkoleOrganisasjon("test") >> { throw new UnableToCreateResourceException("test") }
+        1 * mockService.getSkoleOrganisasjon(bearer) >> { throw new UnableToCreateResourceException("test") }
 
         when:
         def response = mockMvc.perform(get('/api/skoleorganisasjon/')
-                .header(HttpHeaders.AUTHORIZATION, "test"))
+                .header(HttpHeaders.AUTHORIZATION, bearer))
 
         then:
         response.andExpect(status().isBadRequest())
@@ -42,11 +44,11 @@ class ControllerSpec extends MockMvcSpecification {
 
     def "When some backend returns 408 controller should return 408 Request Timeout"() {
         given:
-        1 * mockService.getSkoleOrganisasjon("test") >> { throw new ResourceRequestTimeoutException("test") }
+        1 * mockService.getSkoleOrganisasjon(bearer) >> { throw new ResourceRequestTimeoutException("test") }
 
         when:
         def response = mockMvc.perform(get('/api/skoleorganisasjon/')
-                .header(HttpHeaders.AUTHORIZATION, "test"))
+                .header(HttpHeaders.AUTHORIZATION, bearer))
 
         then:
         response.andExpect(status().isRequestTimeout())
